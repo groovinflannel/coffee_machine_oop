b69650f88d8fbef495a2342c4c8ab10d82eeb329
groovinflannel <groovinflannel@gmail.com>
2023-08-23 13:19:44 -0400

    added get_items and find_drink to menu.py

--- menu.py
+++ menu.py
@@ -9,3 +9,28 @@ class MenuItem:
             "milk": milk,
             "coffee": coffee,
         }
+
+
+class Menu:
+    """Models the menu with drinks on it."""
+
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)
+        ]
+
+    def get_items(self):
+        """Returns the names of menu items"""
+        options = ""
+        for item in self.menu:
+            options += f"{item.name}/"
+        return options
+
+    def find_drink(self, order_name):
+        """Searches the menu for a drink by name. Return the item if it exists, negative statement string if not"""
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+        print("Sorry, that item is not on the menu.")
