32c303c90ff1db6b82dd69ea0ad4988a14b50d7e
groovinflannel <groovinflannel@gmail.com>
2023-08-23 13:10:33 -0400

    added initialization method to menu.py

---
+++ menu.py
@@ -0,0 +1,11 @@
+class MenuItem:
+    "Models an item on the menu."
+
+    def __init__(self, name, water, milk, coffee, cost):
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee,
+        }
