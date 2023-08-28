87e22d642b860d9f731cfb7bfeb2e0b410a8df9b
groovinflannel <groovinflannel@gmail.com>
2023-08-28 14:25:36 -0400

    make_payment method added to money_machine.py

--- money_machine.py
+++ money_machine.py
@@ -17,3 +17,23 @@ class MoneyMachine:
         """Prints profit"""
         print(f"Money available: {self.CURRENCY}{self.profit}")
 
+    def process_coins(self):
+        """Returns total calculated from inserted coins"""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Return True when the payment is successful, False if not"""
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry, not enough money inserted. Issuing refund.")
+            self.money_received = 0
+            return False
