f67fee8d76cac691fbfdb9d09719d52c5d7857fc
groovinflannel <groovinflannel@gmail.com>
2023-08-29 13:09:32 -0400

    imported all files to main.py; can print menu items

---
+++ main.py
@@ -0,0 +1,10 @@
+import coffee_maker
+import money_machine
+import menu
+
+our_coffee_maker = coffee_maker.CoffeeMaker()
+our_money_machine = money_machine.MoneyMachine()
+our_menu = menu.Menu()
+
+
+print(our_menu.get_items())
