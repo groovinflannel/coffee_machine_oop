e821714a1989ab692762de445aaa0802fd4e73fb
groovinflannel <groovinflannel@gmail.com>
2023-08-29 13:59:18 -0400

    can make drinks when running main.py; money machine not yet implemented

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ our_coffee_maker = coffee_maker.CoffeeMaker()
 our_money_machine = money_machine.MoneyMachine()
 our_menu = menu.Menu()
 
+print("Welcome to the OOP Coffee Machine.")
+order = input("What would you like? Type espresso, latte, or cappuccino and press Enter. Type report to get report on "
+              "machine resources or quit to end the program. ")
 
-print(our_menu.get_items())
+our_coffee_maker.report()
+menu_item = our_menu.find_drink(order)
+our_coffee_maker.make_coffee(menu_item)
